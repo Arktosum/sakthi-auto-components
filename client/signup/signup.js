cd8bf1960736f9c023737c10a1c2d662cfc37774
Rag-gy <106253494+Rag-gy@users.noreply.github.com>
2022-10-24 20:32:43 +0530

    added custom alerts
    done custom alerts but have an issue, when clicked on signup in login page or vice versa, i've got a 404 error and messaged you, searched in stack but they said some silly extension and stuff

--- client/signup/signup.js
+++ client/signup/signup.js
@@ -1,5 +1,57 @@
 const dbForm = document.getElementById('db-form');
 
+function alert_(type, message, delay){
+
+    if(document.getElementById("inter")){
+        document.getElementById("inter").remove()
+    }
+
+    const types = {
+        "good" : {
+            "dclass" : "alert alert-success alert-dismissible d-flex align-items-center fade show",
+            "iclass" : "bi-check-circle-fill",
+            "sm" : "Well Done"
+        },
+
+        "error" : {
+            "dclass" : "alert alert-danger alert-dismissible d-flex align-items-center fade show",
+            "iclass" : "bi-exclamation-octagon-fill",
+            "sm" : "Error"
+        },
+
+        "warning" : {
+            "dclass" : "alert alert-warning alert-dismissible d-flex align-items-center fade show",
+            "iclass" : "bi-exclamation-triangle-fill",
+            "sm" : "Warning"
+        }
+    }
+
+    var div1 = document.createElement("div");
+    div1.className = types[type]["dclass"]
+    div1.id="inter";
+
+    var i = document.createElement("i");
+    i.className = types[type]["iclass"]
+
+    var strong = document.createElement("strong");
+    strong.className = "mx-2";
+    strong.innerText = types[type]["sm"];
+
+    var mes = document.createTextNode("    :    "+message);
+    strong.append(mes)
+
+    div1.append(i, strong);
+
+    document.getElementById("alert").appendChild(div1)
+
+    setTimeout(()=>{
+        document.getElementById("inter").remove()
+        console.log("done")
+    }, delay); 
+
+}
+
+
 dbForm.addEventListener('submit',(e)=>{
     e.preventDefault() // overrides default submission. results in error if deleted.
     const data = Object.fromEntries(new FormData(e.target).entries()); // Converts form data into key value pairs for us.
@@ -13,10 +65,9 @@ dbForm.addEventListener('submit',(e)=>{
     }
     fetch("http://localhost:8080/signup",postOptions).then((res)=>res.json()).then((data)=>{
         switch(data.error){
-            case 0 : alert("Successfully created account!");
-                     
-                     break;
-            case -1: alert("Account Already Exists!")
+            case 0 : alert_("good", "Successfully created account!", 1500);
+                    break;
+            case -1: alert_("warning", "Account Already Exists!", 2000)
                     break;
         }
     })
